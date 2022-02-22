5d8f8124b55736731cbddb46746159b7816064a5
Ayberk Sorgun <ayberk@hazelcast.com>
2022-02-22 16:08:05 +0300

    cleanup & adding total row
    Added a TOTAL row to deviation table and applied some cleanup, specified JQL queries

--- jira-data-analysis/cloud_analysis.py
+++ jira-data-analysis/cloud_analysis.py
@@ -7,10 +7,10 @@ import os
 #setting up user-specific parameters
 desktop_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop') 
 jiraOptions = {'server': "https://hazelcast.atlassian.net"}
-jira = JIRA(options=jiraOptions, basic_auth=("COMPANY_E_MAIL", "JIRA_TOKEN"))
+jira = JIRA(options=jiraOptions, basic_auth=("YOUR_COMPANY_EMAIL", "YOUR_TOKEN"))
 
 #insert your query written in JQL
-jira_query = 'INSERT_YOUR_JQL_HERE'
+jira_query = 'project = HZC AND type in (Bug, Story, Task) AND status = Done and originalEstimate > 0 and timespent > 0 ORDER BY created DESC'
 
 
 #run query to get result lenght
@@ -18,7 +18,7 @@ results = jira.search_issues(jql_str=jira_query,maxResults=100000)
 issue_count = results.total
 print("Total issue count: " + str(issue_count))
 iter_count = int(((issue_count - issue_count % 100) / 100) + 1)
-issues = []
+issues_deviation = []
 
 
 #iterate over query to store records in a python list
@@ -26,7 +26,7 @@ while iter_count > 0:
     print("Starting the query from: " + str(((iter_count -1) * 100)))
     for each in jira.search_issues(jql_str= jira_query,startAt=((iter_count -1) * 100),maxResults=100):
        
-        issues.append([
+        issues_deviation.append([
             each.key, 
             each.fields.issuetype.name, 
             #int(each.fields.aggregatetimeestimate), 
@@ -38,9 +38,16 @@ while iter_count > 0:
             each.fields.status.name, 
             each.fields.assignee.displayName])
     iter_count -=1
-    
+
+#filter out the data from some specific users
+for index, line in enumerate(issues_deviation):
+    if line[5] == 'Ozge Avcioglu' or line[5] == 'Former user' or line[5] == 'Alexander Galibey' or line[5] == 'Grzegorz Piwowarek' or line[5] == 'Andrii Borovenskyi' or line[5] == 'Eugene Abramchuk':
+        issues_deviation[index] = None
+
+issues_deviation = list(filter(lambda v: v is not None, issues_deviation))
+
 #convert list to pandas dataframe for easier data manipulation    
-df_issues = pd.DataFrame(issues, columns=["KEY",
+df_issues = pd.DataFrame(issues_deviation, columns=["KEY",
                                           "ISSUE_TYPE",
                                           #"aggregatetimeestimate",
                                           "aggregatetimeoriginalestimate",
@@ -54,6 +61,7 @@ df_issues = pd.DataFrame(issues, columns=["KEY",
 df_issues["aggregatetimeoriginalestimate"] = df_issues["aggregatetimeoriginalestimate"].apply(lambda sec: sec/3600)
 df_issues["timespent"] = df_issues["timespent"].apply(lambda sec: sec/3600)
 df_deviation = pd.pivot_table(df_issues,index=['ASSIGNEE'], aggfunc={'aggregatetimeoriginalestimate': 'sum','timespent': 'sum','KEY':'count'})
+df_deviation.loc["Total"] = df_deviation.sum()
 df_deviation["avg_estimation"]= df_deviation.apply(lambda row: round(row.aggregatetimeoriginalestimate/row.KEY,2), axis=1)
 df_deviation["avg_timespent"]= df_deviation.apply(lambda row: round(row.timespent/row.KEY,2), axis=1)
 df_deviation["deviation_pct"]= df_deviation.apply(lambda row: round((100 * row.timespent/row.aggregatetimeoriginalestimate)-100,2), axis=1)
@@ -63,7 +71,7 @@ df_issues.to_csv(desktop_path + '/deviation_raw_data.csv', sep=',', encoding='ut
 df_deviation.to_csv(desktop_path + '/deviation_table.csv', sep=',', encoding='utf-8')
 
 
-jira_query = 'INSERT_YOUR_JQL_HERE'
+jira_query = 'project = HZC AND type in (Bug) AND status = Done and timespent > 0 ORDER BY created DESC'
 
 
 #run query to get result lenght
@@ -71,7 +79,7 @@ results = jira.search_issues(jql_str=jira_query,maxResults=100000)
 issue_count = results.total
 print("Total issue count: " + str(issue_count))
 iter_count = int(((issue_count - issue_count % 100) / 100) + 1)
-issues = []
+issues_bugs = []
 
 
 #iterate over query to store records in a python list
@@ -79,7 +87,7 @@ while iter_count > 0:
     print("Starting the query from: " + str(((iter_count -1) * 100)))
     for each in jira.search_issues(jql_str= jira_query,startAt=((iter_count -1) * 100),maxResults=100):
        
-        issues.append([
+        issues_bugs.append([
             each.key, 
             each.fields.issuetype.name, 
             #int(each.fields.aggregatetimeestimate), 
@@ -93,7 +101,7 @@ while iter_count > 0:
     iter_count -=1
     
 #convert list to pandas dataframe for easier data manipulation    
-df_bugs = pd.DataFrame(issues, columns=["KEY",
+df_bugs = pd.DataFrame(issues_bugs, columns=["KEY",
                                           "ISSUE_TYPE",
                                           #"aggregatetimeestimate",
                                           #"aggregatetimeoriginalestimate",
